922bf5a20746931d3d56138049dd8109d4f8de67
KoukiSone <l2pzbp99@gmail.com>
2019-06-12 06:07:30 +0900

    学習内容：MySQL

---
+++ MySQL.java
@@ -0,0 +1,41 @@
+package list;
+
+class MySQL {
+
+	/* 以下の基礎的な命令文を学習しました。
+	USE
+	
+	SELECT
+		LIMIT
+		OFFSET
+		SHOW (CREATE TABLE ... , TABLES, DATABASES)
+		DESCRIBE
+		LIKE
+		ORDER BY ....(デフォルト、DESC)
+		SUM,AVG,COUNT
+			GROUP BY
+	
+	WHERE
+		BETWEEN ... and ...
+		in
+		or
+	
+	INSERT
+	UPDATE
+	DELETE
+	
+	
+	START TRANSACTION
+	ROLEBACK
+	COMMIT
+	
+	バッジファイルの作成
+		データベースの作成
+		テーブルの作成	
+	バッヂファイルの実行（SOURCE
+	
+	プライマリキーの設定
+	ALTRER TABLE ... auto_increment = ...
+	*/
+
+}
