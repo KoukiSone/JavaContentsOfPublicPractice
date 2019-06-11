020433c843e73b55f9d763303cb42b9fdab30736
KoukiSone <l2pzbp99@gmail.com>
2019-06-12 06:00:34 +0900

    学習内容：HTML  servlet JSP

---
+++ ListOfJavaEE.java
@@ -0,0 +1,32 @@
+package sample;
+
+public class ListOfJavaEE {
+	//eclipseでのJavaEE環境構築
+	//＜HTML＞
+	//htmlの基本構造（タグ：html、head、body、DOCTYPE
+	//タグ：meta,title,h1,p.a,table
+	//HTMLクイックリファレンス
+	//フォーム、input type、text等
+	//リクエスト送信：GET,POSＴ
+	
+	
+	//＜サーブレットJSP基礎＞
+	//動的webプロジェクト（servlet,JSP,Java,HTMLCSS）
+	//Content-Typeヘッダの設定
+	//サーブレットでHTMLを出力(PrintWriter)
+	//@webアノテーション
+	//doGet(),HttpservletRequest
+	//pageディレクティブ、スクリプトレット、スクリプト式
+	//リクエストパラメータの取得
+	//JSP,MVCモデル
+	//フォワード、JSPファイルへの直接リクエストの禁止方法
+	//JavaBeansルール
+	//リクエストスコープ（生成、保存、取得）
+	//セッションスコープ（〃）
+	//フィルタ（webfilterアノテーションの付与、インターフェース、dofilter）
+	//動的インクルード（includタグJSP向け）
+	//静的インクルード（ディレクティブ）
+	//EL式
+	
+	
+}
